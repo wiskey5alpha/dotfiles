88f9071519935f3479bb4d5fa978b90202ed08b2
Timothy R Aldrich <wiskey5alpha@gmail.com>
2020-04-17 14:27:41 -0700

    Updated editor from vim to emacs

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -1,33 +1,14 @@
 # Editing
 
-export EDITOR=vim
+export EDITOR=edit
 
-# If mvim is installed, use it instead of native vim
-if [[ "$(which mvim)" ]]; then
-    EDITOR="mvim -v"
-    alias vim="$EDITOR"
-fi
 
 if [[ ! "$SSH_TTY" ]]; then
   if [[ ! "$TMUX" ]]; then
-    is_osx && EDITOR=mvim || EDITOR=gvim
+    EDITOR=edit
   fi
   export LESSEDIT="$EDITOR ?lm+%lm -- %f"
-  export GIT_EDITOR="$EDITOR -f"
+  export GIT_EDITOR="$EDITOR"
 fi
 
 export VISUAL="$EDITOR"
-
-function q() {
-  if [[ -t 0 ]]; then
-    $EDITOR "$@"
-  else
-    # Read from STDIN (and hide the annoying "Reading from stdin..." message)
-    $EDITOR - > /dev/null
-  fi
-}
-alias qv="q $DOTFILES/link/.{,g}vimrc +'cd $DOTFILES'"
-alias qs="q $DOTFILES"
-
-# For when you have vim on the brain
-alias :q=exit
