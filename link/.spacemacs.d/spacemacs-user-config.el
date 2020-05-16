dbe1ba3414d42fbaf5c060dd616b1beb99d2bfd7
Timothy R Aldrich <wiskey5alpha@gmail.com>
2020-05-15 17:07:08 -0700

    updated org tags, beginning fresh with agenda views

--- link/.spacemacs.d/spacemacs-user-config.el
+++ link/.spacemacs.d/spacemacs-user-config.el
@@ -16,10 +16,86 @@
 (setq deft-default-extension "org")
 (setq deft-auto-save-interval 30)
 
+;;(with-eval-after-load 'org
+
 (add-hook 'org-mode-hook 'turn-on-auto-fill)
 (add-hook 'org-mode-hook 'flyspell-mode)
+(add-hook 'org-mode-hook (lambda () (hl-todo-mode -1) nil))
 
-(require 'org-protocol)
+(setq org-todo-keywords '(
+        (sequence "TODO(t)" "NEXT(n)" "|" "DONE(d!)")
+))
+(setq org-todo-keyword-faces (quote (
+                      ("TODO" :foreground "brown"      :weight bold)
+                      ("NEXT" :foreground "tomato"     :weight bold)
+                      ("DONE" :foreground "olive drab" :weight bold)
+)))
+
+(setq org-stuck-projects
+'(
+   ;; the tags-todo search that identifies projects
+   "-someday-wait-IGNORE-REVIEW/TODO"
+   ;; if these TODO words are found, it is not stuck
+   ("NEXT")
+   ;; if these tags are found in the subitems, it is not stuck
+   nil
+   ;; a regular expression that matches non stuck
+   ""
+  )
+ )
+
+(with-eval-after-load 'org
+;; Replace org-set-tags with org-set-tags-command in keybinding
+(spacemacs/set-leader-keys-for-major-mode 'org-mode ":" 'org-set-tags-command)
+)
+(setq org-tags-column -120)
+
+(setq org-tag-alist '(
+ ;;   Next Action Contexts
+      ("comms"    .   ?c)
+      ("web"      .   ?w)
+      ("cac"      .   ?a)
+      ("office"   .   ?o)
+      ("home"     .   ?h)
+      ("mcen"     .   ?m)
+      ("vault"    .   ?v)
+      ("imefdm"   .   ?i)
+ ;;   Meetings and People
+      ("staff"    .   ?t)
+      ("spe"      .   ?p)
+      ("col"      .   ?C)
+ ;;   Categories and flags
+      ("someday"  .   ?s)
+      ("wait"     .   ?W)
+      ("read"     .   ?r)
+      ("fifo"     .   ?f)
+      ("journal"  .   ?j)
+      ("REVIEW"   .   ?R)
+      ))
+
+  (setq org-tags-exclude-from-inheritance '(
+      "read"
+      "REVIEW"
+      "someday"
+      "wait"
+      "fifo"
+      "journal"
+      ))
+
+(org-clock-persistence-insinuate)
+;; the number of clock tasks to remember in history
+(setq org-clock-history-length 36)
+;;  resume clock when clocking into task with open clock.
+;; When clocking into a task with a clock entry which has not been closed,
+;; the clock can be resumed from that point
+(setq org-clock-in-resume t)
+;; when set to t , both the running clock and entire history are saved when
+;; emacs closes and resume when emacs restarts
+(setq org-clock-persist t)
+;; put clock times into LOGBOOK drawer
+(setq org-clock-into-drawer t)
+;; clock out when the task is marked DONE
+(setq org-clock-out-when-done t)
 
 ;;;; http://stackoverflow.com/questions/23517372/hook-or-advice-when-aborting-org-capture-before-template-selection
 
@@ -36,39 +112,40 @@
       (delete-frame)))
 
 ; set the default directory for some org functionality
-   (setq org-directory "~/org")
-   (setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
-   (setq org-refile-allow-creating-parent-nodes 'confirm)
+(setq org-directory "~/org")
+(setq org-agenda-files (file-expand-wildcards "~/org/*.org"))
+(setq org-refile-allow-creating-parent-nodes 'confirm)
 
 
-   (defun rebuild-agenda-files ()
-     (interactive)
-     (load-library "find-lisp")
-     (setq org-agenda-files (directory-files "~/org" t "\.org$"))
-     (setq org-agenda-text-search-extra-files
-           (append
-;            (find-lisp-find-files "~/org/reference" "\.org$")
-;            (find-lisp-find-files "~/org/posts"     "\.org$")
-;            (find-lisp-find-files "~/org/sites"     "\.org$")
-;            (find-lisp-find-files "~/org/journals"  "\.org$")
-;            (find-lisp-find-files "~/org/support"   "\.org$")
-            (find-lisp-find-files "~/org/notes"     "\.org$")
-            )
-           )
-     (setq tra:all-org-files
-           (append
-            org-agenda-files
-            org-agenda-text-search-extra-files)
-           )
-     (setq org-refile-targets
-           (quote ((nil :maxlevel . 5)
-                   (tra:all-org-files :maxlevel . 5)
-           ))
-     )
-   )
+(defun rebuild-agenda-files ()
+  (interactive)
+  (load-library "find-lisp")
+  (setq org-agenda-files (directory-files "~/org" t "\.org$"))
+  (setq org-agenda-text-search-extra-files
+        (append
+         (find-lisp-find-files "~/org/notes"     "\.org$")
+         )
+        )
+  (setq tra:all-org-files
+        (append
+         org-agenda-files
+         org-agenda-text-search-extra-files)
+        )
+  (setq org-refile-targets
+        (quote ((nil :maxlevel . 5)
+                (tra:all-org-files :maxlevel . 5)
+        ))
+  )
+)
+(rebuild-agenda-files)
 
-
-   (rebuild-agenda-files)
+(setq org-agenda-custom-commands
+   '(("s" "Stuck Projects"
+       ((org-ql-block '(and (not (done))
+                            (not (descendants (todo "NEXT")))
+                            (not (descendants (todo "TODO")))
+                            (not (descendants (scheduled))))
+                      ((org-ql-block-header "Stuck Projects")))))))
 
 (setq org-archive-location "~/org/.archive/%s_archive::datetree/")
 
@@ -129,6 +206,8 @@
  (quote ((org-agenda-files :maxlevel . 7)
                       (nil :maxlevel . 7))))
 (setq org-refile-use-outline-path (quote file))
+
+;;)
 
 (defun review ()
   (interactive)
