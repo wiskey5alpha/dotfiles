75e303881daa13e11351a914229879cabe7cf4fd
Timothy Aldrich <wiskey5alpha@gmail.com>
2020-11-30 16:46:53 -0800

    fixed dropbox being started on startup

--- config/awesome/configuration/apps.lua
+++ config/awesome/configuration/apps.lua
@@ -34,6 +34,7 @@ return {
     'ibus-daemon --xim --daemonize', -- Ibus daemon for keyboard
     'numlockx on', -- enable numlock
     '/usr/lib/policykit-1-gnome/polkit-gnome-authentication-agent-1 & eval $(gnome-keyring-daemon -s --components=pkcs11,secrets,ssh,gpg)', -- credential manager
+    'dropbox start',
     -- Add applications that need to be killed between reloads
     -- to avoid multipled instances, inside the awspawn script
     '~/.config/awesome/configuration/awspawn' -- Spawn "dirty" apps that can linger between sessions
