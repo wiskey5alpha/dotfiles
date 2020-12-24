3974f332b461c00734557319b2dccd394325e927
Timothy Aldrich <wiskey5alpha@gmail.com>
2020-12-23 16:42:27 -0800

    a _big_ wrapper around emacsclient

--- config/fish/functions/edt.fish
+++ config/fish/functions/edt.fish
@@ -2,6 +2,19 @@
 function edt -d "run the emacsclient"
     ##  a wrapper around the emacsclient with lots of options.
 
+    # TODO: I want to make this easier by hiding all the other options
+    #       behind these function options {{{
+    # - n :: open an new gui frame (default)
+    # - c :: open in an active gui frame
+    # - t :: open in terminal
+    # - :: connect to stdin
+    # load any files that were passed in
+    #
+    # with a '-', pipe stdin to emacs
+    # if there are any other options send them to the client unchanged
+    #
+    # }}}
+
     # emacsclient options {{{
     # -nw, -t, --tty 	....................  Open a new Emacs frame on the current terminal
     # -c, --create-frame .................  Create a new frame instead of trying to
@@ -22,33 +35,24 @@ function edt -d "run the emacsclient"
     #                                       for locating files remotely via Tramp
     # }}}
 
-    # function options {{{
-    # without any options (other than a file name), open a new GUI frame.
-    # with a '-', pipe stdin to emacs
-    # if there are any other options send them to the client unchanged
-    #
-    # }}}
-
     set -l emacs "/usr/bin/emacsclient"
-    set -l emacs_options  '-a ""' # make sure to start the server if not already
+    set -l emacs_options  -a '""'   # make sure to start the server if not already
     set -l is_running     0       # assume it is not running until tested
     set -l use_terminal   0       # assume we want to use the gui
 
-    # check if emacsclient is already running
-    if pgrep -U (id -u) emacsclient > /dev/null
+    # check if emacs server is already running
+    if pgrep -U (id -u) -f 'emacs --daemon' > /dev/null
         set $is_running 1
     end
 
-    # We'll need the '-c' if there are no arguments or if there's no client
-    if test (count $argv) -eq 0; or test $is_running -eq 1
-        # there were no options past in
+    # We'll need the '-c' if the server isnt running already
+    if test $is_running -eq 1
         set -a emacs_options "-c"
     end
 
     # There are arguments passed in, process those ...
     if test (count $argv) -gt 0
         for arg in $argv
-            echo "processing $arg"
             switch $arg
                 case "-nw" "-t" "-tty"
                     # looking to stay in the terminal
@@ -64,14 +68,16 @@ function edt -d "run the emacsclient"
                     set -a emacs_options "(delete-file \"'$TMP'\"))')\""
                 case "*"
                     # its either one of the lesser used options, or filenames now
+                    # or maybe it's -c and we want a new window
                     set -a emacs_options $arg
             end
         end
     end
-    # if called without arguments - open a new gui instance
-    if $use_terminal -eq 1
-        command $emacs $emacs_options
-    else
-        command $emacs $emacs_options &
+    if test $use_terminal -eq 0
+        # we aren't using the terminal so return immediately.
+        # so we dont need an '&' at the end
+        set -a emacs_options "-n"
     end
+    ##    echo $emacs $emacs_options
+    command $emacs $emacs_options
 end
